2b76fa5d3de58dc8a51998679250bcc5f8c58e49
noOneCodes42 <117050732+noOneCodes42@users.noreply.github.com>
2025-07-11 17:40:55 -0700

    Setting Up Project

---
+++ United Hackathon V5/Cheerio/Cheerio/CheerioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheerioApp.swift
+//  Cheerio
+//
+//  Created by Neel Arora on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CheerioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
